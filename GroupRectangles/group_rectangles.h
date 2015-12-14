83dd6e440b259ff2fbc3bdac98d0a26c0f737a67
jsvirzi <jsvirzi@gmail.com>
2015-12-13 21:08:28 -0500

    include file for group_rectangles.c

---
+++ GroupRectangles/group_rectangles.h
@@ -0,0 +1,11 @@
+#ifndef GROUP_RECTANGLES_H
+#define GROUP_RECTANGLES_H
+
+typedef struct rectangle {
+	float x1, y1, x2, y2;
+} Rectangle; 
+
+Rectangle *group_rectangles(Rectangle *rects, int n, float beta, int *size);
+char *print_rectangle(Rectangle *rect);
+
+#endif
