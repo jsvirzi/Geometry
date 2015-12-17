085b43c67bcdbd2fab8c8a968b5a33cb3ae0d0f5
jsvirzi <jsvirzi@gmail.com>
2015-12-17 17:34:15 -0500

    snapshot

--- GroupRectangles/test_group_rectangles.c
+++ GroupRectangles/test_group_rectangles.c
@@ -23,16 +23,47 @@ Rectangle rectangles[] = {
 	// {  9.0,  9.0, 15.0, 15.0 }, 
 	// { 11.0,  8.0, 13.0, 13.0 },
 
+	{  1.0,  1.0,  4.0,  4.0 },
+	{  2.0,  3.0,  3.0,  4.0 },
+
+	{  5.0,  1.0,  8.0,  4.0 },
+	{  7.0,  2.0,  8.0,  3.0 },
+
+	{  9.0,  1.0, 12.0,  4.0 },
+	{ 10.0,  1.0, 11.0,  2.0 },
+
+	{  1.0,  5.0,  4.0,  8.0 },
+	{  1.0,  6.0,  2.0,  7.0 },
+
+	{  5.0,  5.0,  7.0,  8.0 },
+	{  6.0,  5.0,  8.0,  8.0 },
+
+	{  9.0,  5.0, 12.0,  8.0 },
+	{ 10.0,  5.0, 11.0,  8.0 },
+
+	{  1.0,  9.0,  4.0, 12.0 },
+	{  1.0,  9.0,  2.0, 10.0 },
+
+	{  5.0,  9.0,  8.0, 12.0 },
+	{  7.0,  9.0,  8.0, 10.0 },
+
+	{  9.0,  9.0, 12.0, 12.0 },
+	{ 11.0, 11.0, 12.0, 12.0 },
+
+	{  1.0, 13.0,  4.0, 16.0 },
+	{  1.0, 15.0,  2.0, 16.0 },
+
 	{ -1.0,  0.0,  0.0,  0.0 }
 };
 
-float scale = 0.0;
+float xscale = 0.0, yscale = 0.0;
 
 int main(int argc, char **argv) {
 	int i, size, nrects = 0;
 	for(nrects=0;rectangles[nrects].x1>=0;) ++nrects; /* count the rectangles */
-	for(i=0;i<nrects;++i) if(rectangles[i].y2 > scale) scale = rectangles[i].y2;
-	printf("found %d rectangles as input. scale = %.1f\n", nrects, scale);
+	for(i=0;i<nrects;++i) if(rectangles[i].y2 > yscale) yscale = rectangles[i].y2;
+	for(i=0;i<nrects;++i) if(rectangles[i].x2 > xscale) xscale = rectangles[i].x2;
+	printf("found %d rectangles as input. scales = %.1f, %.1f\n", nrects, xscale, yscale);
 	Rectangle *new_rectangles = group_rectangles(rectangles, nrects, 0.1, &size);
 	printf("%d old rectangles\n", nrects);
 	for(i=0;i<nrects;++i) printf("%s\n", print_rectangle(&rectangles[i]));
@@ -42,25 +73,26 @@ int main(int argc, char **argv) {
 	int w = 350, h = 250;
 	Mat frame = Mat::zeros(h, w, CV_8UC3);
 
-	Scalar old_color = Scalar(0xff, 0x00, 0x00);
+	Scalar old_color[2] = { Scalar(0xff, 0x00, 0x00), Scalar(0x00, 0xff, 0x00) };
 	Scalar new_color = Scalar(0x00, 0x00, 0xff);
 
 	for(i=0;i<size;++i) {
-		float x = w * new_rectangles[i].x1 / scale; 
-		float y = h * new_rectangles[i].y1 / scale; 
-		float dx = w * (new_rectangles[i].x2 - new_rectangles[i].x1) / scale; 
-		float dy = h * (new_rectangles[i].y2 - new_rectangles[i].y1) / scale; 
+		float x = w * new_rectangles[i].x1 / xscale; 
+		float y = h * new_rectangles[i].y1 / yscale; 
+		float dx = w * (new_rectangles[i].x2 - new_rectangles[i].x1) / xscale; 
+		float dy = h * (new_rectangles[i].y2 - new_rectangles[i].y1) / yscale; 
 		Rect rect(x, y, dx, dy);
 		cv::rectangle(frame, rect, new_color, 3);
 	}
 
 	for(i=0;i<nrects;++i) {
-		float x = w * rectangles[i].x1 / scale; 
-		float y = h * rectangles[i].y1 / scale; 
-		float dx = w * (rectangles[i].x2 - rectangles[i].x1) / scale; 
-		float dy = h * (rectangles[i].y2 - rectangles[i].y1) / scale; 
+		int index = i % 2;
+		float x = w * rectangles[i].x1 / xscale; 
+		float y = h * rectangles[i].y1 / yscale; 
+		float dx = w * (rectangles[i].x2 - rectangles[i].x1) / xscale; 
+		float dy = h * (rectangles[i].y2 - rectangles[i].y1) / yscale; 
 		Rect rect(x, y, dx, dy);
-		cv::rectangle(frame, rect, old_color, 1);
+		cv::rectangle(frame, rect, old_color[index], 1 + index);
 	}
 
 	namedWindow("main", 1);
