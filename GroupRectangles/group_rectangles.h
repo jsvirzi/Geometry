50f9e0270aefa0760596f6528174ffcba6cc51f3
jsvirzi <jsvirzi@gmail.com>
2015-12-21 02:19:22 -0500

    option to use preallocated memory to store results

--- GroupRectangles/group_rectangles.h
+++ GroupRectangles/group_rectangles.h
@@ -5,7 +5,7 @@ typedef struct rectangle {
 	float x1, y1, x2, y2;
 } Rectangle; 
 
-Rectangle *group_rectangles(Rectangle *rects, int n, float beta, int *size);
+Rectangle *group_rectangles(Rectangle *rects, int n, float beta, int *size, Rectangle *results = 0);
 char *print_rectangle(Rectangle *rect);
 
 #endif
