1884c16f6b8020c7be9eb7658fa542555c9a644c
jsvirzi <jsvirzi@gmail.com>
2016-12-05 03:08:17 -0800

    testing. is cost function properly calculated? not getting true minimum

--- Regression/src/regression.cpp
+++ Regression/src/regression.cpp
@@ -8,8 +8,18 @@
 #include <stdio.h>
 #include <math.h>
 
-float cost(float *x, float *y, int n, float m, float b) {
+float cost(float *x, float *y, int n, float m, float b, int option) {
+	float S = 0.0f;
+	for(int i=0;i<n;++i) {
+		S += fabs(m * x[i] - y[i] + b);
+	}
+	if(option == 1) {
+		S = S / sqrt(1.0f + m * m);
+	}
+	return S;
 }
+
+/* minimal code between here */
 
 float regression(float *x, float *y, int n, float *slope, float *intercept, int option) {
 	float mux = 0.0f, muy = 0.0f, muxx = 0.0f, muxy = 0.0f, muyy = 0.0f;
@@ -30,8 +40,7 @@ float regression(float *x, float *y, int n, float *slope, float *intercept, int 
 	float sxx = muxx - mux * mux;
 	float sxy = muxy - mux * muy;
 	float m, b;
-	if (option == 0) {
-// printf("sxy = %f. sxx = %f\n", sxy, sxx);
+	if(option == 0) {
 		m = sxy / sxx;
 	} else {
 		float syy = muyy - mux * muy;
@@ -42,14 +51,12 @@ float regression(float *x, float *y, int n, float *slope, float *intercept, int 
 	*slope = m;
 	*intercept = b;
 
-/* calculate the cost function */
-	float S = 0.0f;
-	for(int i=0;i<n;++i) {
-		S += fabs(m * x[i] - y[i] + b);
-	}
-	S = S / sqrt(1.0f + m * m);
-	return S;
+	// return -1.0f;
+	/* uncomment the following line instead to return the cost function */
+	return cost(x, y, n, m, b, option);
 }
+
+/* and here */
 
 #ifdef TEST
 
@@ -63,33 +70,38 @@ int main(int argc, char **argv) {
 	std::normal_distribution<> dist(1.0, 0.05);
 
 	float m, b, S;
+	float mTrue = 1.0, bTrue = 0.0, T;
 
 	/* no errors on x. only equal errors on y */
 	for(int i=0;i<N;++i) {
 		float abscissa = i;
 		x[i] = i; 
-		y[i] = i + dist(gen) - 1.0f;
+		y[i] = mTrue * i + dist(gen) - 1.0f;
 		printf("random value %d = (%f, %f)\n", i, x[i], y[i]);
 	}
 
 	S = regression(x, y, N, &m, &b, 0);
-	printf("linear regression gives y = %.3f x + %.3f. S = %f\n", m, b, S);
+	T = cost(x, y, N, mTrue, bTrue, 0);
+	printf("linear regression gives y = %.3f x + %.3f. S = %f. T = %f\n", m, b, S, T);
 	S = regression(x, y, N, &m, &b, 1);
-	printf("normal regression gives y = %.3f x + %.3f. S = %f\n", m, b, S);
+	T = cost(x, y, N, mTrue, bTrue, 1);
+	printf("normal regression gives y = %.3f x + %.3f. S = %f. T = %f\n", m, b, S, T);
 
 	/* equal errors on x, y */
 	for(int i=0;i<N;++i) {
 		float abscissa = i;
 		x[i] = i + dist(gen) - 1.0f; 
-		// y[i] = i + dist(gen) - 1.0f;
 		printf("random value %d = (%f, %f)\n", i, x[i], y[i]);
 	}
 
 	S = regression(x, y, N, &m, &b, 0);
-	printf("linear regression gives y = %.3f x + %.3f. S = %f\n", m, b, S);
+	T = cost(x, y, N, mTrue, bTrue, 0);
+	printf("linear regression gives y = %.3f x + %.3f. S = %f. T = %f\n", m, b, S, T);
 	S = regression(x, y, N, &m, &b, 1);
-	printf("normal regression gives y = %.3f x + %.3f. S = %f\n", m, b, S);
+	T = cost(x, y, N, mTrue, bTrue, 1);
+	printf("normal regression gives y = %.3f x + %.3f. S = %f. T = %f\n", m, b, S, T);
 
+	return 0;
 }
 
 #endif
